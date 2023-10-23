2cc1619e53302669acbd803f1c896076b1f40b2d
Saimonses <Simonky_6@hotmail.com>
2023-10-23 14:41:56 -0300

    Initial commit

---
+++ app/src/main/java/com/log/sensores/MainActivity.java
@@ -0,0 +1,29 @@
+package com.log.sensores;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void openProximity(View view) {
+        startActivity(new Intent(MainActivity.this,
+                ProximityActivity.class));
+    }
+    public void openGyroscope(View view) {
+        startActivity(new Intent(MainActivity.this,
+                GyroscopeActivity.class));
+    }
+    public void openRotationVector(View view) {
+        startActivity(new Intent(MainActivity.this,
+                RotationVectorActivity.class));
+    }
+
+}
